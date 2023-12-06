deacb3203d4abd737c0cbcb2add30bcfdaabc438
Elmer Balbin <contact@elmer.dev>
2023-12-06 14:12:25 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Web Dev 2 - jQuery Exercises
+
+The instructions are inside the `app.js` file. Follow each instruction per exercise.
---
+++ app.js
@@ -0,0 +1,37 @@
+//
+// Follow the instructions per exercise. You can only use one selector line per exercise.
+// For example:
+// CORRECT:
+//   $('.container').css('background', 'green').html('Some html')
+// INCORRECT:
+//   $('.container').css('background', 'green')
+//   $('.container').html('Some html')
+//
+
+$(function() {
+
+  // .exercise-1 - Change the height to 300px and background color to pink
+  $('.exercise-1')
+
+
+  // .exercise-2 - Change the background color of <span>C</span> to red. You cannot use :nth-child or :last-child
+  $('.exercise-2')
+
+
+  // .exercise-3 - Add a <span>A</span> before <span>B</span> and add a <span>E</span> after <span>D</span>
+  $('.exercise-3')
+
+
+  // .exercise-4 - Change the text to 'Hello world!' and change the font size to 30px
+  $('.exercise-4')
+
+
+  // .exercise-5 - Add a click event handler to the button that shows an alert popup with the text 'Hello' when clicked
+  $('.exercise-5')
+
+
+  // .exercise-6 - Add a hover event handler so that when you hover over .exercise-6 the background turns to black. When you hover out, it goes back to grey
+  $('.exercise-6')
+
+  
+})
